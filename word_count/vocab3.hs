580c15fec090088b0616ed4b5d3641f7edadb8f7
niccolovettorello1997 <n.vettorello@cartaidentitalimentare.com>
2022-11-18 17:25:50 +0100

    chapter 1 paragraph 4

--- word_count/vocab3.hs
+++ word_count/vocab3.hs
@@ -14,7 +14,7 @@ extractVocabulary :: T.Text -> Vocabulary
 extractVocabulary t = map buildEntry $ group $ sort ws
  where
   ws = map T.toCaseFold $ filter (not . T.null) $ map cleanWord $ T.words t
-  buildEntry xs@(x:_) = (x, length x)
+  buildEntry xs@(x:_) = (x, length xs)
   buildEntry [] = error "unexpected empty list"
   cleanWord = T.dropAround (not . isLetter)
 
@@ -28,7 +28,7 @@ allWords vocabulary = map fst vocabulary
 wordsCount :: Vocabulary -> (Int, Int)
 wordsCount vocabulary = (sum $ map snd vocabulary, length vocabulary)
 
--- sort the vocabulary by comparing frequencies by reversed order
+-- sort the vocabulary in descending order by comparing frequencies
 wordsByFrequency :: Vocabulary -> Vocabulary
 wordsByFrequency = sortBy (comparing $ Down . snd)
 
@@ -38,4 +38,21 @@ wordsCountReport vocabulary = T.unlines [part1, part2]
  where
   (total, unique) = wordsCount vocabulary
   part1 = T.append (T.pack "Total number of words: ") (T.pack $ show total)
-  part2 = T.append (T.pack "Unique number of words: ") (T.pack $ show unique) 
+  part2 = T.append (T.pack "Unique number of words: ") (T.pack $ show unique)
+
+-- get the first n most frequent words
+frequentWords :: Vocabulary -> Int -> T.Text
+frequentWords vocabulary n = T.unlines $ take n (map fst $ wordsByFrequency vocabulary)
+
+-- print to console the first n most frequent words
+processTextFile :: FilePath -> Int -> IO ()
+processTextFile fname n = do
+ text <- TIO.readFile fname
+ TIO.putStrLn $ frequentWords (wordsByFrequency $ extractVocabulary text) n
+
+main :: IO()
+main = do
+ args <- getArgs
+ case args of
+  [fname, num] -> processTextFile fname (read num)
+  _ -> putStrLn "Usage: vocab3 filename frequent_words_number"
